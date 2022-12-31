a30277934fbddc0b2f4104b670e8074cbf2e48c4
YuriyMitrofanov <yurasell@yandex.ru>
2022-12-31 15:59:13 +0300

    Refresh state, sent attributes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,32 +8,38 @@ export const Counter = (props) => {
   // Т.о "props" - это объект с элементами, равными заданным при ренедринге атрибутами.
   // Ну и в данном случае я могу, используя "props" задать начальное значение для значения
   // задаваемой переменной.
-  let [count, setCount] = useState(props.value)
+  // Также в props можем передавать информацию, на основе которой будут рендериться дочерние элементы данного компонента.
+  // В частности, св-во "counters.name" передает в дочерний элемент <span> компонента "Counter" значение {props.name} (см. return)
+  // Т.е. в данном случае "props.name = counters.name";  также как  "props.value = counters.value"
+  console.log(props)
+  let [value, satValue] = useState(props.value)
 
   function formatCount(){
-    return count === 0 ? "empty" : count
+    return value === 0 ? "empty" : value
   }
   const getBadgeClasses = () => {
     let classes = "badge m-2 "
-    classes += count === 0 ? "bg-warning" : "bg-primary"
+    classes += value === 0 ? "bg-warning" : "bg-primary"
     return classes
   }
 
   function handleIncrement(){
-    setCount((prevState) => prevState + 1)
-    console.log(count)
+    satValue((prevState) => prevState + 1)
+    console.log(value)
   }
 
   function handleDecrement(){
-    setCount((prevState) => prevState - 1)
-    console.log(count)
+    satValue((prevState) => prevState - 1)
+    console.log(value)
   }
 
     return (
       <div>
+        <span>{props.name}</span>
         <span className = { getBadgeClasses() }> { formatCount() } </span>
         <button className = 'btn btn-primary btn-sm m-1' onClick = { handleIncrement }> + </button>
         <button className = 'btn btn-primary btn-sm m-1' onClick = { handleDecrement }> - </button>
+        <button className="btn btn-danger btn-sm m-2" onClick = {() => props.onDelete(props.id)}>Delete</button>
       </div>
     )
   }
--- src/components/countrsList.jsx
+++ src/components/countrsList.jsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import { Counter } from './counter'
 
 export const CountersList = () => {
     // Массив со свойствами каждого из счетчиков (массив исходнынх данных)
-    const counters = [
-        {id: 0, value: 0}, 
-        {id: 1, value: 4}, 
-        {id: 2, value: 0}, 
-        {id: 3, value: 0}
-    ]
+    const [counters, setCounters] = useState([
+        {id: 0, value: 0, name: 'Ложка', price: '150'}, 
+        {id: 1, value: 1, name: 'Вилка'}, 
+        {id: 2, value: 0, name: 'Тарелка', material: 'porcelaine'}, 
+        {id: 3, value: 0, name: 'Набор минималиcта'}
+    ])
+    const handleDelete = (id) => {
+        const newCounters = counters.filter(counter => counter.id!==id)
+        setCounters(newCounters)
+    }
 
     return (
         <>
             {counters.map(counter => (
-                <Counter key={counter.id} value={counter.value} />
+                <Counter 
+                    key={counter.id} 
+                    {...counter}
+                    onDelete = {handleDelete}/>
             ))}
         </>
     )
